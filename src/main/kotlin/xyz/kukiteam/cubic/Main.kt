47bf05f7b9723f3fbeccd2131184c5f474c68314
LambdaXCF <nobodyminer@outlook.com>
2019-03-15 22:43:23 +0100

    Basic window setup

--- src/main/kotlin/xyz/kukiteam/cubic/Main.kt
+++ src/main/kotlin/xyz/kukiteam/cubic/Main.kt
@@ -1,8 +1,104 @@
 package xyz.kukiteam.cubic
 
+import org.lwjgl.Version
+import org.lwjgl.glfw.Callbacks
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFW.GLFW_FALSE
+import org.lwjgl.glfw.GLFW.GLFW_TRUE
+import org.lwjgl.glfw.GLFW.glfwWindowShouldClose
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11
+import org.lwjgl.system.MemoryStack.stackPush
+import org.lwjgl.system.MemoryUtil.NULL
+
+
 class Main {
+
+    companion object {
+        private var window: Long = -1L
+    }
+
+    fun run() {
+        println("Hello LWJGL " + Version.getVersion() + "!")
+
+        init()
+        loop()
+
+        // Free the window callbacks and destroy the window
+        Callbacks.glfwFreeCallbacks(window)
+        GLFW.glfwDestroyWindow(window)
+
+        GLFW.glfwTerminate()
+        GLFW.glfwSetErrorCallback(null)!!.free()
+    }
+
+    private fun init() {
+        GLFWErrorCallback.createPrint(System.err).set()
+
+        if (!GLFW.glfwInit())
+            throw IllegalStateException("Unable to initialize GLFW")
+
+        GLFW.glfwDefaultWindowHints() // optional, the current window hints are already the default
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
+
+        // Create the window
+        window = GLFW.glfwCreateWindow(1024, 600, "Hello World!", NULL, NULL)
+        if (window == NULL)
+            throw RuntimeException("Failed to create the GLFW window")
+
+
+        GLFW.glfwSetKeyCallback(window) { window, key, scancode, action, mods ->
+            if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE)
+                GLFW.glfwSetWindowShouldClose(window, true)
+        }
+
+        stackPush().use { stack ->
+            val pWidth = stack.mallocInt(1)
+            val pHeight = stack.mallocInt(1)
+
+            GLFW.glfwGetWindowSize(window, pWidth, pHeight)
+
+            val vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())
+
+            GLFW.glfwSetWindowPos(
+                    window,
+                    (vidmode!!.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            )
+        }
+
+        // Make the OpenGL context current
+        GLFW.glfwMakeContextCurrent(window)
+
+        // Enable v-sync
+        GLFW.glfwSwapInterval(1)
+
+        GLFW.glfwShowWindow(window)
+    }
+
+    private fun loop() {
+        GL.createCapabilities()
+
+        // Set the clear color
+        GL11.glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while (!glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT or GL11.GL_DEPTH_BUFFER_BIT) // clear the framebuffer
+
+            GLFW.glfwSwapBuffers(window) // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            GLFW.glfwPollEvents()
+        }
+    }
+
 }
 
 fun main() {
-    Main()
+    Main().run()
 }
